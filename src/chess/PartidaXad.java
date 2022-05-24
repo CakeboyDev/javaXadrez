9345f2c231123fca52adebe24fff39acd0d62976
Emerson Estevan Crespo <emerthedev@gmail.com>
2022-05-23 23:48:27 -0300

    Pequena melhoria na impressão do tabuleiro

--- src/chess/PartidaXad.java
+++ src/chess/PartidaXad.java
@@ -7,30 +7,40 @@ import chess.pecas.Torre;
 public class PartidaXad {
 
 	private Tabuleiro tabs;
-	
+
 	public PartidaXad() {
-		tabs = new Tabuleiro(8,8);
+		tabs = new Tabuleiro(8, 8);
 		setupInicial();
 	}
-	
-	public PecaXad[][] getPecas(){
+
+	public PecaXad[][] getPecas() {
 		PecaXad[][] mat = new PecaXad[tabs.getRows()][tabs.getColumns()];
-		for(int i=0;i<tabs.getRows();i++) {
-			for(int j=0;j<tabs.getColumns();j++) {
-				mat[i][j]= (PecaXad)tabs.pec(i,j);
+		for (int i = 0; i < tabs.getRows(); i++) {
+			for (int j = 0; j < tabs.getColumns(); j++) {
+				mat[i][j] = (PecaXad) tabs.pec(i, j);
 			}
 		}
 		return mat;
 	}
-	
+
 	private void posicionarNovaPeca(char column, int row, PecaXad peca) {
-		tabs.posicPeca(peca, new PosicXad(column,row).toPosic());
+		tabs.posicPeca(peca, new PosicXad(column, row).toPosic());
 	}
-	
+
 	private void setupInicial() {
-		posicionarNovaPeca('b',6,new Torre(tabs, Cor.PRETO));
-		posicionarNovaPeca('e',8,new Rei(tabs, Cor.PRETO));
-		posicionarNovaPeca('e',1,new Rei(tabs, Cor.PRETO));
+		posicionarNovaPeca('c', 1, new Torre(tabs, Cor.BRANCO));
+		posicionarNovaPeca('c', 2, new Torre(tabs, Cor.BRANCO));
+		posicionarNovaPeca('d', 2, new Torre(tabs, Cor.BRANCO));
+		posicionarNovaPeca('e', 2, new Torre(tabs, Cor.BRANCO));
+		posicionarNovaPeca('e', 1, new Torre(tabs, Cor.BRANCO));
+		posicionarNovaPeca('d', 1, new Rei(tabs, Cor.BRANCO));
+
+		posicionarNovaPeca('c', 7, new Torre(tabs, Cor.PRETO));
+		posicionarNovaPeca('c', 8, new Torre(tabs, Cor.PRETO));
+		posicionarNovaPeca('d', 7, new Torre(tabs, Cor.PRETO));
+		posicionarNovaPeca('e', 7, new Torre(tabs, Cor.PRETO));
+		posicionarNovaPeca('e', 8, new Torre(tabs, Cor.PRETO));
+		posicionarNovaPeca('d', 8, new Rei(tabs, Cor.PRETO));
 	}
-	
+
 }
